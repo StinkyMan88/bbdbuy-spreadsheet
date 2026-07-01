71b47995903afe8053d8f2722948f2847842ae9d
StinkyMan88 <detr0ytm5@gmail.com>
2026-07-01 12:27:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTx0FJMWiBJuzWwo6MyxVLvzf8G0fUyhC5_xLZGDhZugFiX2Nav4_Bh4QTEUcO8kRoVxhd8QjNFuyBK/pub?output=csv";
+const container = document.getElementById("products");
 
 async function loadProducts() {
-    const response = await fetch(SHEET_URL);
-    const csv = await response.text();
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbws9-UFKiodBc6IQm4J9ThzaiSU6zokjIyiW6sinPeb2A4nngqnNQAYoV_Ev0nXKy8V/exec"
+    );
 
-    const rows = csv.trim().split("\n");
+    const text = await response.text(); // primero como texto (más seguro)
+    const data = JSON.parse(text);
 
-    console.log(rows);
+    console.log("API DATA:", data);
+
+    container.innerHTML = "";
+
+    data.forEach(product => {
+      const card = document.createElement("div");
+      card.style.border = "1px solid #ccc";
+      card.style.padding = "10px";
+      card.style.margin = "10px 0";
+
+      card.innerHTML = `
+        <h3>${product.name}</h3>
+        <p>${product.price}</p>
+        <a href="${product.link}" target="_blank">View item</a>
+      `;
+
+      container.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error("Error:", error);
+    container.innerHTML = "Error loading products";
+  }
 }
 
 loadProducts();
